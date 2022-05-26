44ff1d7a31dd562d0ac3a40b34f9c03507708ace
Zenrool <lorenzopapperini@hotmail.it>
2022-05-26 12:01:00 +0200

    Copertina

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -3,9 +3,19 @@
     class="d-flex flex-column align-items-center p-1"
     :class="isMovie ? 'movieCard' : 'seriesCard'"
   >
+    <img
+      class="movieImage"
+      :src="
+        movie.poster_path
+          ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
+          : require('../assets/img/poster.png')
+      "
+      alt="Movie image"
+    />
     <h2>{{ cardName }}</h2>
     <p>{{ cardOName }}</p>
     <img
+      class="flag"
       v-if="flagsIs"
       :src="require(`../assets/img/${movie.original_language}.svg`)"
       alt="flag"
@@ -49,7 +59,7 @@ li.seriesCard {
   border: 3px solid green;
 }
 li {
-  height: 300px;
+  height: 400px;
   h2,
   p {
     text-align: center;
@@ -57,10 +67,14 @@ li {
   p {
     font-size: 1.5rem;
   }
-  img {
-    width: 60px;
+  .flag {
+    width: 30px;
     height: auto;
     border: 1px solid black;
+  }
+  .movieImage {
+    height: 50%;
+    width: auto;
   }
 }
 </style>
